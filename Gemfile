fb500696aa7bd8f2f1fe68a798d66226b8e78a4e
Mark Wales <mark@smallhadroncollider.com>
2017-08-20 11:14:05 +0100

    added capistrano deployment file

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'https://rubygems.org'
+
+group :development do
+    gem 'capistrano-composer'
+    gem 'capistrano-laravel', '>= 0.0.3', :git => 'https://github.com/smallhadroncollider/capistrano-laravel.git'
+end
---
+++ deployment/deploy.rb
@@ -0,0 +1,26 @@
+# config valid only for current version of Capistrano
+set :application, "restful.training"
+set :repo_url, "git@github.com:smallhadroncollider/restful-blog.git"
+
+set :scm, :git
+
+# Default value for :linked_files is []
+set :linked_files, fetch(:linked_files, []).push(".env")
+
+# Default value for linked_dirs is []
+set :linked_dirs, fetch(:linked_dirs, []).push("storage")
+
+namespace :deploy do
+    after :updating, "laravel:create_paths"
+    after :updating, "composer:install_executable"
+    after :published, "laravel:optimize"
+
+    task :php_reload do
+        on roles(:app) do
+            info "Restarting PHP-FPM"
+            execute "sudo service php7.0-fpm reload"
+        end
+    end
+
+    after :published, "php_reload"
+end
