d34699866eac51560e0553f10c1e5f2e0ffb68e1
Mark Wales <mark@smallhadroncollider.com>
2018-05-15 11:48:07 +0100

    added Tag resource

--- app/Http/Controllers/Tags.php
+++ app/Http/Controllers/Tags.php
@@ -6,20 +6,26 @@ use Illuminate\Http\Request;
 use App\Account;
 use App\Tag;
 use App\Article;
+use App\Http\Resources\TagResource;
+use App\Http\Resources\ArticleResource;
 
 class Tags extends Controller
 {
     public function list(Account $account)
     {
-        return Tag::select("tags.*")->leftJoin("article_tag", "tag_id", "tags.id")
+        $tags = Tag::select("tags.*")->leftJoin("article_tag", "tag_id", "tags.id")
             ->leftJoin("articles", "articles.id", "article_id")
             ->where("articles.account_id", $account->id)
             ->groupBy("tags.id")
             ->get();
+
+        return TagResource::collection($tags);
     }
 
     public function articles(Account $account, Tag $tag)
     {
-        return $tag->articles->where("account_id", $account->id)->all();
+        $articles = $tag->articles->where("account_id", $account->id);
+
+        return ArticleResource::collection($articles);
     }
 }
