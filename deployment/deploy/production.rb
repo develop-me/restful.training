b8ca36014bb2634d4ab22113950374a726fdced8
Mark Wales <mark@developme.tech>
2020-09-25 10:38:34 +0100

    updates deployment with Laravel caching tasks

--- deployment/deploy/production.rb
+++ deployment/deploy/production.rb
@@ -2,7 +2,3 @@ server "smallhadroncollider.com", user: "mark", roles: %w{app}
 
 set :branch, "master"
 set :deploy_to, "/var/www/restful.training"
-
-namespace :deploy do
-    after :published, "laravel:migrate_db"
-end
